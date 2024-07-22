6c1a7d083e130a2526272690dc8d6d1744b0d962
Griffin Kowash <griffin.kowash@ema3d.com>
2024-07-22 14:00:59 -0600

    compute velocity magnitude when reading FEM results

--- src/ema/testing/data_reader.py
+++ src/ema/testing/data_reader.py
@@ -44,9 +44,20 @@ class FEMReader(DataReader):
 	def load(self, filepath):
 		# Load time steps and field data
 		t, data_dict = load_charge_results(filepath)
+		data_dict = self._process_vector_quantities(data_dict)
+
 		return {field : {'x': t,
 						 'y': data_dict[field],
 						 'ymin': np.min(data_dict[field], axis=0),
 						 'ymean': np.mean(data_dict[field], axis=0),
 						 'ymax': np.max(data_dict[field], axis=0)
 						 } for field in data_dict}
+
+	def _process_vector_quantities(self, data_dict):
+		"""Checks for certain vector quantities and computes magnitude if present."""
+
+		# Velocity
+		if 'Vx' in data_dict and 'Vy' in data_dict and 'Vz' in data_dict:
+			data_dict['V'] = np.sqrt(data_dict['Vx']**2 + data_dict['Vy']**2 + data_dict['Vz']**2)
+
+		return data_dict
